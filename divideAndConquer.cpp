d2e15d278ced1c1bb45128aef94bb803e454cb93
peng <peng.yuan2029@gmail.com>
2018-04-26 16:27:41 -0700

    DC

---
+++ divideAndConquer.cpp
@@ -0,0 +1,41 @@
+//DC
+//ECS 122A Spring 2018
+//Yuan
+
+#include "../yuanHeadAll.h" 
+using namespace std;
+
+int Counting_Inversions(vector<int> & nums){
+
+}
+
+class Counting_Inversions
+{
+private:
+	vector<int> nums;
+
+	int count(int left, int right){
+		if(left == right)
+			return 0;
+
+		int 
+	}
+public:
+	int count(vector<int> & n){
+		nums = n;
+		int sz = n.size();
+		return count(0, sz-1)
+	}
+
+	/* data */
+};
+int main(){
+	MCS("algorithm", "logarithm");
+	// return 0;
+
+	vector<int> info{5,6,3,7,5,3,4};
+	cout << matrix_chain_mult(info) << endl;
+
+	return 0;
+}
+
